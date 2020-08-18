712981ac75cc8e69287ffbf9cefa35d8ab96cc58
laurstone <67934225+laurstone@users.noreply.github.com>
2020-08-17 21:47:45 -0400

    Update person.rb

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,16 @@
 class Person
   #your code here
+  
+  attributes = {name: "Sarah", eye_color: "Green"}
+  
+  sarah = Person.send(sarah_attributes)
+  sarah.name
+  sarah.eye_color
+  
+  wendy_attributes = {name: "Wendy", hair_color: "Brown", handed: "Left"}
+  
+  wendy = Person.send(wendy_attributes)
+  wendy.name
+  wendy.hair_color
+  wendy.handed
 end
